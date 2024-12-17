8088463cecf2c110f2a1314a7f929beb722925b7
Honyzey <briac35.v@gmail.com>
2024-12-18 00:42:27 +0100

    update bdd 3

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,4 +1,3 @@
-// models/index.js
 const sequelize = require('../config/db');
 const User = require('./User');
 const Annonce = require('./Annonce');
@@ -9,25 +8,25 @@ const ConversationParticipant = require('./ConversationParticipant');
 const Message = require('./Message');
 
 // Définir les associations entre les modèles
-Annonce.belongsTo(User, { foreignKey: 'user_id' });
-User.hasMany(Annonce, { foreignKey: 'user_id' });
+Annonce.belongsTo(User, { foreignKey: 'user_id', foreignKeyConstraints: false });
+User.hasMany(Annonce, { foreignKey: 'user_id', foreignKeyConstraints: false });
 
-Annonce.belongsTo(Categorie, { foreignKey: 'categorie_id' });
-Categorie.hasMany(Annonce, { foreignKey: 'categorie_id' });
+Annonce.belongsTo(Categorie, { foreignKey: 'categorie_id', foreignKeyConstraints: false });
+Categorie.hasMany(Annonce, { foreignKey: 'categorie_id', foreignKeyConstraints: false });
 
-Annonce.hasMany(Image, { foreignKey: 'annonce_id' });
-Image.belongsTo(Annonce, { foreignKey: 'annonce_id' });
+Annonce.hasMany(Image, { foreignKey: 'annonce_id', foreignKeyConstraints: false });
+Image.belongsTo(Annonce, { foreignKey: 'annonce_id', foreignKeyConstraints: false });
 
-Message.belongsTo(Conversation, { foreignKey: 'conversation_id' });
-Conversation.hasMany(Message, { foreignKey: 'conversation_id' });
+Message.belongsTo(Conversation, { foreignKey: 'conversation_id', foreignKeyConstraints: false });
+Conversation.hasMany(Message, { foreignKey: 'conversation_id', foreignKeyConstraints: false });
 
-Message.belongsTo(User, { as: 'sender', foreignKey: 'sender_id' });
+Message.belongsTo(User, { as: 'sender', foreignKey: 'sender_id', foreignKeyConstraints: false });
 
-Conversation.belongsTo(Annonce, { foreignKey: 'annonce_id' });
-Annonce.hasMany(Conversation, { foreignKey: 'annonce_id' });
+Conversation.belongsTo(Annonce, { foreignKey: 'annonce_id', foreignKeyConstraints: false });
+Annonce.hasMany(Conversation, { foreignKey: 'annonce_id', foreignKeyConstraints: false });
 
-Conversation.belongsToMany(User, { through: ConversationParticipant, foreignKey: 'conversation_id' });
-User.belongsToMany(Conversation, { through: ConversationParticipant, foreignKey: 'user_id' });
+Conversation.belongsToMany(User, { through: ConversationParticipant, foreignKey: 'conversation_id', foreignKeyConstraints: false });
+User.belongsToMany(Conversation, { through: ConversationParticipant, foreignKey: 'user_id', foreignKeyConstraints: false });
 
 module.exports = {
     sequelize,
